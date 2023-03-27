e0c681ab27710e1a4d117e93d6ec6256bedea0b1
Pierre Gallusci <19683800+gskyu@users.noreply.github.com>
2023-03-27 20:53:15 +0200

    Added blazor project and base files.

---
+++ Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Gskyu.SyncedListPicker.Models;
+
+public class Item
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public bool Completed { get; set; }
+}
---
+++ Pages/SyncedListPicker.razor.cs
@@ -0,0 +1,52 @@
+﻿using Gskyu.SyncedListPicker.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Gskyu.SyncedListPicker.Pages;
+
+public partial class SyncedListPicker : ComponentBase
+{
+    protected List<Item> ItemList { get; set; }
+
+    protected EditContext AddItemContext { get; set; }
+    protected Item ItemToAdd { get; set; }
+
+    protected string RandomlyChosenOne { get; set; }
+
+    public SyncedListPicker()
+    {
+        ItemToAdd = new Item { Id = Guid.NewGuid().ToString() };
+        AddItemContext = new EditContext(ItemToAdd);
+
+        ItemList ??= new List<Item>();
+
+        RandomlyChosenOne = string.Empty;
+    }
+
+    protected void AddItem()
+    {
+        ItemList.Add(new Item
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = ItemToAdd.Name,
+            Completed = false
+        });
+    }
+
+    protected void UpdateItem(string id)
+    {
+        Console.WriteLine("called update");
+    }
+
+    protected void PickRandomItem(bool completedIncluded)
+    {
+        var rnd = new Random(DateTime.UtcNow.Millisecond);
+        RandomlyChosenOne = (completedIncluded ? ItemList : ItemList.Where(i => !i.Completed).ToList())
+            [rnd.Next(0, ItemList.Count - 1)].Name;
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+}
